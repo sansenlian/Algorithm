1f742fc60dc9bb3da92c34f4ebd7eccb10b22480
AchunOrigin <sansenlian@foxmail.com>
2019-08-13 11:53:40 +0800

    更新
    更新

---
+++ Search/KMP字符串查找.md
@@ -0,0 +1,12 @@
+# KMP字符串查找
+
+字符串A中找是否存在字符串B
+
+当然前提A比B长
+
+![](https://raw.githubusercontent.com/sansenlian/PicPlace/master/img/20190812101649.png)
+
+![](https://raw.githubusercontent.com/sansenlian/PicPlace/master/img/20190812102019.gif)
+
+动图来自:[https://blog.csdn.net/weixin_40346046/article/details/79468142](https://blog.csdn.net/weixin_40346046/article/details/79468142)
+
--- 题库题解/力扣/算法初级/数组/旋转数组.js
+++ 题库题解/力扣/算法初级/数组/旋转数组.js
@@ -1,17 +1,26 @@
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * @param {string[]} strs
+ * @return {string}
  */
-var isAnagram = function(s, t) {
-    let len_t = t.length;
-    let len_s = s.length;
-    if(len_s!=len_t)return false;
-    let _map = new Map();
-    
+var longestCommonPrefix = function(strs) {
+    let len = strs.length;
+    if(len<=1)return "";
+    let min = strs[0].length;
+    console.log(len)
+    for (let i = 0; i < len; i++) {
+        strs[i]<min?min=strs[i]:mi=min;
+    }
+    for (let i = 0; i < min; i++) {
+        let count = 0;
+
+        if(map.get(strs[0])!=len)return
+    }
+    return "";
+
 };
-let arr = ["h", "e", "l", "l", "o"];
-console.log(firstUniqChar("loveleetcode"))
+let arr = ["flower","flow","flight"];
+console.log(longestCommonPrefix(arr))
+//console.log(strStr("ababcabcacbab", "abaabcac"));
 
 function f() {
     let len = nums.length;
